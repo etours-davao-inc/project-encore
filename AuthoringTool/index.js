d405ac5762008d1e5358140640896379b297f1ad
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-09-19 15:59:56 +0800

    Import ActivityCanvas

--- AuthoringTool/index.js
+++ AuthoringTool/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Product from './Product';
 
 import { Provider } from './Context';
+
+import Product from './Product';
+import ActivityCanvas from './ActivityCanvas';
 
 export default () => {
   return (
     <Provider>
       <Product />
+      <ActivityCanvas />
     </Provider>
   )
 }
