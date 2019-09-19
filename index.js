079485891c72733da4016cf986aeeb973d31e6ef
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-09-19 14:55:49 +0800

    Import Authoring Tool

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import AuthoringTool from './AuthoringTool';
+
 export default {
   mount: () => {
     console.log('here')
-    const Hello = () => (<h1>Hello, Isabel</h1>);
     const root = document.getElementById('root');
-    ReactDOM.render(<Hello />, root);
+    ReactDOM.render(<AuthoringTool />, root);
   }
 }
 
